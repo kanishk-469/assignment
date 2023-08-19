522e8e4af83c386dc362fff62bc72b85b968fe43
kanishk-469 <kanishk.singh469@gmail.com>
2023-08-19 15:12:45 +0530

    footer issue resolved

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,14 @@
-document.querySelector('.js-grid-image-and-text')
-.innerHTML = `
-<div class="grid-image-and-text">
-<div class="yoga-image-div-container-invisible">
-      <img src="./assets/carring.jpg" alt="yoga-image" class="yoga-image">
-</div>
-    <div class="image-and-text-div"><span class="image-and-text-heading">What is Lorem Ipum?<br></span>
-    <p class="image-and-text-sub-heading">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.</p>
-    
-     <p class="image-and-text-paragraph"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-     <p class="image-and-text-paragraph"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-     <p class="image-and-text-paragraph"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-     <p class="image-and-text-paragraph"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-    </div>
-    <div class="yoga-image-div-container">
-      <img src="./assets/carring.jpg" alt="yoga-image" class="yoga-image">
-    </div>
-    </div>
-`;
+// document.querySelector('.js-grid-image-and-text')
+// .innerHTML = `
 
-document.querySelector('.js-grid-image-and-text-2')
-.innerHTML = `
-<div class="grid-image-and-text-2">
-      <div class="yoga-image-div-container">
-        <img src="./assets/Jar.jpg" alt="yoga-image" class="jar-image">
-      </div>
-     <div class="image-and-text-div"><span class="image-and-text-heading">What is Lorem Ipum?<br></span>
-     <p class="image-and-text-sub-heading">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.</p>
-     
-      <p class="image-and-text-paragraph right-spacing"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-      <p class="image-and-text-paragraph right-spacing"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-      <p class="image-and-text-paragraph right-spacing"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-      <p class="image-and-text-paragraph right-spacing"> &#x2022; elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p> 
-     </div>
-     </div>
-`;
+// `;
+
+// document.querySelector('.js-grid-image-and-text-2')
+// .innerHTML = `
+
+// `;
 
    document.getElementById('footer-brand')
    .innerHTML =`
     <div class="trademark-text">BRAND &#174;and Mysite logo are registered trademarks of Mysite AB, used under license by Mysite Inc.<br> &#169; 2023 Mysite.All rights reserved.</div>
-   
    `;
